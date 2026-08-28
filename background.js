04fa300efc4658835eb6cd97996c3cc4115eaa7e
KaanEnnes <mrkaan840@gmail.com>
2026-08-28 15:45:41 +0300

    background.js: paralel arama destegi

--- background.js
+++ background.js
@@ -2,4 +2,8 @@ chrome.runtime.onMessage.addListener((msg) => {
   if (msg && msg.type === "snrOpenIncognito" && msg.url) {
     chrome.windows.create({ url: msg.url, incognito: true });
   }
+  // Paralel arama işçileri için: her işçi kendi sekmesinde çalışır.
+  if (msg && msg.type === "snrOpenTab" && msg.url) {
+    chrome.tabs.create({ url: msg.url, active: false });
+  }
 });
